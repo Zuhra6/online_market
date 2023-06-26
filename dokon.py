0298f9d4924698615e2b0cc3b4f1cf1cf5364044
Zuhra6 <137803878+Zuhra6@users.noreply.github.com>
2023-06-26 21:00:20 +0500

    update pro 26.06.2023
    update pro 26.06.2023

--- dokon.py
+++ dokon.py
@@ -1,18 +1,14 @@
-# import pandas lib as pd
+
 import pandas as pd
 
-# read by default 1st sheet of an excel file
 df = pd.read_excel('data.xlsx')
 
-# print(df['Experence'].value_counts())
 df_result = df
-# df_result.drop(labels='bithday', axis=1, inplace=True)
 
 
 df2 = df[df['first_name'] == 'Bella']
-# print(df2)
+
 
 df_result.drop(df2.index, inplace=True)
 print(df_result[df_result['first_name'] == 'Bella'])
-# df_result.to_excel('data_result.xlsx', index=False)
 
